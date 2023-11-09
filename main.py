1327531937524b2cb68a191c1d2d08f0a13041bd
yassinex02 <tahayassinemoumni@gmail.com>
2023-11-09 18:08:25 +0100

    hi

---
+++ main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+client = OpenAI()
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant for university students."},
+        {"role": "user", "content": "Why should I join IE University's AI Hackathon?"}
+    ],
+    temperature=0.7
+)
+
+print(response.choices[0].message.content)
